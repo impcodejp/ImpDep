070f6d6da6d9f19ca1ced724bd4c2ad58a77af4e
ryosuke-takiguchi <ryosuke_takiguchi@impcode.net>
2026-03-15 22:58:04 +0900

    release

--- src-tauri/src/models/budget.rs
+++ src-tauri/src/models/budget.rs
@@ -1,10 +1,10 @@
 use sqlx::types::BigDecimal;
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(serde::Deserialize)]
 pub struct BudgetInput {
     pub start_date_of_application: i32,
-    pub gross_profit_budget: BigDecimal,
-    pub new_gross_profit_budget: BigDecimal,
-    pub max_load_score: BigDecimal,
+    pub gross_profit_budget: Option<BigDecimal>,     // 💡 Option に変更
+    pub new_gross_profit_budget: Option<BigDecimal>, // 💡 Option に変更
+    pub max_load_score: Option<BigDecimal>,          // 💡 Option に変更
 }
